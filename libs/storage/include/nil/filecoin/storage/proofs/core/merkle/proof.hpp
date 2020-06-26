99af5561861fa9a1da8b7d3646459d0410418258
Mikhail Komarov <nemo@nil.foundation>
2020-06-26 22:34:01 +0300

    Implementation updated.

--- libs/storage/include/nil/filecoin/storage/proofs/core/merkle/proof.hpp
+++ libs/storage/include/nil/filecoin/storage/proofs/core/merkle/proof.hpp
@@ -27,7 +27,85 @@
 #define FILECOIN_STORAGE_PROOFS_CORE_MERKLE_PROOF_HPP
 
 namespace nil {
-    namespace filecoin { }
+    namespace filecoin {
+        /// Trait to abstract over the concept of Merkle Proof.
+        template<typename Hash, typename Arity = PoseidonArity, typename SubTreeArity = PoseidonArity,
+                 typename TopTreeArity = PoseidonArity>
+        struct MerkleProofTrait {
+            typedef Hash hash_type;
+            typedef Arity arity_type;
+            typedef SubTreeArity subtree_arity;
+            typedef TopTreeArity top_tree_arity;
+
+            /// Try to convert a merkletree proof into this structure.
+            static MerkleProofTrait<Hash, Arity, SubTreeArity, TopTreeArity>
+                try_from_proof(const Proof<typename Hash::domain_type, Arity> &p) {
+            }
+
+            std::vector<std::pair<std::vector<Fr>, std::size_t>> as_options(&self) {
+                return path()
+                    .iter()
+                    .map(| v | {(v .0.iter().copied().map(Into::into).map(Some).collect(), Some(v .1), )})
+                    .collect::<Vec<_>>();
+            }
+
+            std::pair<Fr, std::vector<std::pair<std::vector<Fr>, std::size_t>>> into_options_with_leaf() {
+                let leaf = self.leaf();
+                let path = self.path();
+                (Some(leaf.into()),
+                 path.into_iter()
+                     .map(| (a, b) | {(a.iter().copied().map(Into::into).map(Some).collect(), Some(b), )})
+                     .collect::<Vec<_>>(), )
+            }
+
+            std::vector<std::pair<std::vector<Fr>, std::size_t>> as_pairs() {
+                self.path()
+                    .iter()
+                    .map(| v | (v .0.iter().copied().map(Into::into).collect(), v .1))
+                    .collect::<Vec<_>>();
+            }
+
+            virtual bool verify() const = 0;
+
+            /// Validates the MerkleProof and that it corresponds to the supplied node.
+            ///
+            /// TODO: audit performance and usage in case verification is
+            /// unnecessary based on how it's used.
+            bool validate(std::size_t node) {
+                if (!verify()) {
+                    return false;
+                }
+
+                return node == self.path_index();
+            }
+
+            bool validate_data(&self, data : <Self::Hasher as Hasher>::Domain) {
+                if (!self.verify()) {
+                    return false;
+                }
+
+                return leaf() == data;
+            }
+
+            fn leaf(&self)-><Self::Hasher as Hasher>::Domain;
+            fn root(&self)-><Self::Hasher as Hasher>::Domain;
+            fn len(&self)->usize;
+            fn path(&self)->Vec<(Vec << Self::Hasher as Hasher > ::Domain >, usize)>;
+
+            std::size_t path_index() {
+                return path().iter().rev().fold(0, | acc, (_, index) | (acc * Self::Arity::to_usize()) + index);
+            }
+
+            bool proves_challenge(std::size_t challenge) {
+                self.path_index() == challenge;
+            }
+
+            /// Calcluates the exected length of the full path, given the number of leaves in the base layer.
+            std::size_t expected_len(std::size_t leaves) {
+                compound_path_length::<Self::Arity, Self::SubTreeArity, Self::TopTreeArity>(leaves)
+            }
+        }
+    }    // namespace filecoin
 }    // namespace nil
 
 #endif
